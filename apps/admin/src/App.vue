c379c483b4c469d8e80fd6ddab885e345fa02877
shilili <18262272638@163.com>
2025-11-04 15:20:27 +0800

    feat: 简化

--- apps/admin/src/App.vue
+++ apps/admin/src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
 import TodoList from './components/TodoList.vue'
 </script>
 
 <template>
+  <HelloWorld />
   <TodoList />
 </template>
 
