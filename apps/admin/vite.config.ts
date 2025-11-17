55d73fdc3de42bf5a5bf7504ec8da846749d8aca
shilili <18262272638@163.com>
2025-11-17 16:09:34 +0800

    feat: 一种链接本地库的方式

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -6,7 +6,19 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@antv/g2': fileURLToPath(new URL('../G2/esm/index.js', import.meta.url))
+    }
+  },
+  optimizeDeps: {
+    exclude: ['@antv/g2']
+  },
+  server: {
+    fs: {
+      allow: [
+        fileURLToPath(new URL('.', import.meta.url)),
+        fileURLToPath(new URL('../G2', import.meta.url))
+      ]
     }
   }
 })
